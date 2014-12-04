63c5e96c17a6cc356bb2147d19f0ffca1400786e
Jesse Farmer <jesse@codeunion.io>
2014-12-03 22:25:58 -0800

    Move mothership codeunion command to its own file

--- lib/codeunion/command.rb
+++ lib/codeunion/command.rb
@@ -1,34 +1,7 @@
 require "codeunion"
-require "ptools"
 
 module CodeUnion
-  # This class serves as a simple wrapper around subcommands
-  class Command
+  module Command
     class CommandNotFound < StandardError; end
-
-    def initialize(command, *args)
-      @command = command
-      @args = args
-    end
-
-    def exist?
-      File.which(executable_name)
-    end
-
-    def exec!
-      if exist?
-        exec(executable_name, *args)
-      else
-        fail CommandNotFound, "Unknown command `#{@command}`."
-      end
-    end
-
-    private
-
-    attr_reader :command, :args
-
-    def executable_name
-      "codeunion-#{command}"
-    end
   end
 end
