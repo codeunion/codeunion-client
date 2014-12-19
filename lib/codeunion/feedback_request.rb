dd85f289b2559e360938c36080508efdb3752221
Zee Spencer <zee@zeespencer.com>
2014-12-18 23:18:53 -0800

    Rubocop is happy with CodeUnion::FeedbackRequest

--- lib/codeunion/feedback_request.rb
+++ lib/codeunion/feedback_request.rb
@@ -1,15 +1,15 @@
 require "uri"
-require 'codeunion/github_api'
-require 'addressable/uri'
+require "codeunion/github_api"
+require "addressable/uri"
 
 module CodeUnion
+  # Sends feedback requests to CodeUnion for review
   class FeedbackRequest
-    WEB_URL_REGEX = /\A#{URI::regexp(['http', 'https'])}\z/
+    WEB_URL_REGEX = /\A#{URI.regexp(['http', 'https'])}\z/
 
-    GIT_URL_REGEX = /\A(git:\/\/)?git@github.com:(.*)(.git)?\z/
+    GIT_URL_REGEX = %r{\A(git://)?git@github.com:(.*)(.git)?\z}
     REPO_CAPTURE_INDEX = 1
 
-    DEFAULT_OWNER="codeunion"
     MISSING_ARTIFACT = "You must provide something to provide feedback on"
     INVALID_ARTIFACT = "The artifact provided was not a web URL. We only provide feedback on code hosted online."
 
@@ -34,19 +34,23 @@ module CodeUnion
     def errors
       errors = []
       errors.push(MISSING_ARTIFACT) if artifact.empty?
-      errors.push(INVALID_ARTIFACT) unless is_url?(artifact)
+      errors.push(INVALID_ARTIFACT) unless url?(artifact)
 
       errors.join("\n")
     end
 
     private
-    def is_url?(url)
+
+    def url?(url)
       url =~ WEB_URL_REGEX
     end
 
+    # Ensures a feedback repository location string is usable via the github api
     class FeedbackRepository
+      DEFAULT_OWNER = "codeunion"
+
       def self.coerce_to_proper_url(location)
-        self.new(location).to_s
+        new(location).to_s
       end
 
       def initialize(location)
@@ -58,18 +62,28 @@ module CodeUnion
       end
 
       private
-      CLEAN = ->(location) { location.gsub(/^\//,"").gsub(".git","") }
+
+      CLEAN = ->(location) { location.gsub(/^\//, "").gsub(".git", "") }
       EXTRACTORS = [
-        { :match => ->(location) { location =~ GIT_URL_REGEX },
-          :extract => ->(location) {
+        {
+          :match => ->(location) { location =~ GIT_URL_REGEX },
+          :extract => lambda do |location|
             match = location.match(GIT_URL_REGEX)
-            CLEAN.call(match.captures[REPO_CAPTURE_INDEX]) } },
-        { :match => ->(location) { location =~ WEB_URL_REGEX },
-          :extract => ->(location) { CLEAN.call(URI(location).path) } },
-        { :match => ->(location) { location.split(/\//).length == 2 },
-          :extract => ->(location) { location } },
-        { :match => ->(location) { true },
-          :extract => ->(location) { "#{DEFAULT_OWNER}/#{location}" } }
+            CLEAN.call(match.captures[REPO_CAPTURE_INDEX])
+          end
+        },
+        {
+          :match => ->(location) { location =~ WEB_URL_REGEX },
+          :extract => ->(location) { CLEAN.call(URI(location).path) }
+        },
+        {
+          :match => ->(location) { location.split(/\//).length == 2 },
+          :extract => ->(location) { location }
+        },
+        {
+          :match => ->(_) { true },
+          :extract => ->(location) { "#{DEFAULT_OWNER}/#{location}" }
+        }
       ]
       def extract_owner_and_repo
         EXTRACTORS.each do |extractor|
