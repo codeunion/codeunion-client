51530e9dbdd1a598dc1b50143e832c3717ff955e
Zee Spencer <zee@zeespencer.com>
2014-12-18 23:25:10 -0800

    Rubocop is happy with CodeUnion::Command

--- lib/codeunion/command.rb
+++ lib/codeunion/command.rb
@@ -2,14 +2,17 @@ require "codeunion"
 
 module CodeUnion
   module Command
+    # Raised when a command doesn't exist
     class CommandNotFound < StandardError; end
-    class MissingConfig < StandardError;
-      def initialize(required_fields)
-        super("Make sure you've `codeunion config --set` the following config variables: #{required_fields.join", "}")
-      end
 
+    # Raised when config does not have required fields for a command
+    class MissingConfig < StandardError
+      def initialize(required_fields)
+        super("Make sure you've `codeunion config --set` the following config variables: #{required_fields.join(", ")}")
+      end
     end
+
+    # Raised when input isn't appropriate for a command
     class InvalidInput < StandardError; end
   end
-
 end
